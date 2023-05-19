de47b1b11d939edb9112d2c7511c7f423af96869
Yoo DongWon <thd301302@gmail.com>
2023-05-19 17:15:19 +0900

    DynamicURL

---
+++ src/components/Movie.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
+const Movie = ({ id, poster, title, release, overview }) => {
+  return (
+    <div>
+      <img src={poster} alt={title} style={{ width: "200px" }} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{release}</p>
+      <p>{overview}</p>
+      {/* <ul>
+    {movie.genres.map((g) => (
+      <li key={g}>{g}</li>
+    ))}
+  </ul> */}
+    </div>
+  );
+};
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  poster: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  release: PropTypes.string.isRequired,
+  overview: PropTypes.string.isRequired,
+};
+
+export default Movie;
+
+//     src={`https://image.tmdb.org/t/p/original${poster_path}`}
---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ja-JP&page=1&sort_by=popularity.desc&api_key=27d409977772952380d3c511e12e7d93`
+      )
+    ).json();
+    setMovies(json.results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>loading..</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              poster={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              title={movie.title}
+              release={movie.release_date}
+              overview={movie.overview}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+export default Home;
