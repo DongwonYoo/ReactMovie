3397f30bec0e428cee64f83586bbda3760f2f817
Yoo DongWon <thd301302@gmail.com>
2023-05-25 00:22:18 +0900

    App.js修正

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import About from "./components/About";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 const App = () => {
-  useEffect(() => {
-    const currentPath = window.location.pathname;
-    if (currentPath !== "/home") {
-      window.history.pushState({}, "", "/home");
-    }
-  }, []);
-
   return (
     <Router>
       <Switch>
-        <Route exact path="/home">
-          <Home />
-        </Route>
         <Route path="/movie/:id">
           <Detail />
         </Route>
         <Route path="/about">
           <About />
         </Route>
-        <Redirect to="/home" />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route>
+          <Home />
+        </Route>
       </Switch>
     </Router>
   );
